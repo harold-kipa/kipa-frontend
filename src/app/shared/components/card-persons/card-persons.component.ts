d79bf37fe279e6724026558b119fb1baf910c29c
harold-kipa <harold@k-tedra.co>
2026-08-27 17:09:16 -0500

    Seccion Nosotros terminada

---
+++ src/app/shared/components/card-persons/card-persons.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-persons',
+  imports: [CommonModule],
+  templateUrl: './card-persons.component.html',
+  styleUrl: './card-persons.component.scss',
+})
+export class CardPersonsComponent {
+  @Input() urlImg: string = '';
+  @Input() name: string = '';
+  @Input() occupation: string = '';
+  @Input() label: any[] = [];
+  @Input() description: string = '';
+}
