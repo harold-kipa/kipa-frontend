ee902b222b43c748a12954e1e904fdfe93fcf8db
LittlePrincess2004 <paula.prieto@kipa.expert>
2026-09-02 12:00:39 -0500

    Creacion de trayectoria y botones

---
+++ src/app/features/consultancy/consultancy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardIdeasComponent } from '../../shared/components/card-ideas/card-ideas.component';
+
+@Component({
+  selector: 'app-consultancy',
+  imports: [CardIdeasComponent],
+  templateUrl: './consultancy.component.html',
+  styleUrl: './consultancy.component.scss',
+})
+export class ConsultancyComponent {
+
+}
